226665fb8c82c3bc8015eb5743fad081709511b7
Mage_Enderman <qtfaulkner@gmail.com>
2026-02-24 16:16:54 -0500

    chore: remove spark folder to prepare for submodule

--- spark/docs/overrides/javascript/menu-toggle.js
+++
@@ -1,9 +0,0 @@
-const menuTrigger = document.getElementById("menu-toggle");
-const drawerToggle = document.querySelector(
-  'input[type=checkbox][data-md-toggle="drawer"]'
-);
-if (menuTrigger && drawerToggle) {
-  menuTrigger.addEventListener("click", () => {
-    drawerToggle.checked = !drawerToggle.checked;
-  });
-}
